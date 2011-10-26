49b3c3d6437ca96351401c6bdb59bd4e90169dea
Natapone Charsombut <dong@abctech-thailand.com>
2011-10-26 09:58:20 +0700

    html template

---
+++ fizz_buzz.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Fizz Buzz</title>
+</head>
+<body>
+    <form action="fizz_buzz.php" method="get">
+        input: <input type="text" name="input" />
+        <input type="submit" value="calculate" />
+    </form>
+</body>
+</html>
